2236b414353a0401e15c857b3fe02357dede191d
jasv <jasv1994@gmail.com>
2025-04-09 22:57:19 -0400

    se agrego el README

--- obj/Debug/net48/PrintControl.AssemblyInfo.cs
+++ obj/Debug/net48/PrintControl.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrintControl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a5830db0ec817c21b5aa1483005ffa64087330")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrintControl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrintControl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
